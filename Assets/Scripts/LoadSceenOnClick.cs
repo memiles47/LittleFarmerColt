66197e9f26e43adcb55aaa63080a56ba61992a3e
Michael Miles <boat081062@gmail.com>
2015-04-14 12:06:53 -0500

    Chapter 6 Review
    No nit picking here at all. This chapter is well written easy to follow
    and completely accurate to the function of Unity 5. Zero complaints

---
+++ Assets/Scripts/LoadSceenOnClick.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadSceenOnClick : MonoBehaviour
+{
+    public void LoadScene(int scene)
+    {
+        Application.LoadLevel(scene);
+    }
+}
